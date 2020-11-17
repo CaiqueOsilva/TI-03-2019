3f07ffcb158cd26049644ecacac0d848c248f8be
CaiqueOsilva <57421068+CaiqueOsilva@users.noreply.github.com>
2020-11-17 20:19:27 -0300

    Add files via upload

---
+++ 01-atualizando.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "Atualizando as listas do APT
+    apt update"
+
+    echo -e "atualizando os software"
+    apt -y upgrade
+
+echo -e "Forçando a atualizaçao do sistema"
+ apt -y full-upgrade
+
+ echo -e "Forçando a atualizçao de segurança"
+    apt -y dit-upgrade
+
+echo -e "removendo software desnecessarios"
+     apt -y autoremove
+     apt -y autoclean
+     apt -y clean
+
+     echo -e "sistema atualizado, pressione <Enter> para"
+     read
+     sleep 5
+     reboot 
